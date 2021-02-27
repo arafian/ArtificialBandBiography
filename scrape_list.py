ab549ac963f1880cfad94365fc6b3e83071d195f
James Ngo <james.hoang.ngo@gmail.com>
2021-02-27 12:40:54 -0800

    Add scrape_list.py

---
+++ scrape_list.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import requests
+import sys
+import re
+
+try:
+	url = sys.argv[1]
+except:
+	exit()
+
+response = requests.get(url)
+soup = BeautifulSoup(response.content, "html.parser")
+
+for a in soup.find_all("a", href=True):
+	if a['href'][:6] == "/wiki/" and not ":" in a['href']:
+		suburl = "https://en.wikipedia.org" + a['href']
+		subresponse = requests.get(suburl)
+		subsoup = BeautifulSoup(subresponse.content, "html.parser")
+		first_sentence = subsoup.select("p")[0].getText().split(".")[0]
+		if re.search(r'\bband\b', first_sentence):
+			print(suburl)
